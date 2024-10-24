025f97abdf877678f0ceb05ee2694318bb703aa5
Andrew Kim <andrew.igk@gmail.com>
2024-10-24 11:12:11 -0400

    Add documentation

--- backend/models/SubmittedGym.js
+++ backend/models/SubmittedGym.js
@@ -1,6 +1,8 @@
-/* Schema model for Gym, must be modified */
 const mongoose = require('mongoose')
 
+/** Schema model for gym submissions.
+ *
+ */
 const SubmittedGymSchema = new mongoose.Schema({
   latitude: { type: Number, required: true },
   longitude: { type: Number, required: true },
