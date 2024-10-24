a0bf63e17ccc64515be0111a64884a4fab97123a
Andrew Kim <andrew.igk@gmail.com>
2024-10-24 10:12:35 -0400

    Implement user model and controller

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,20 @@
-/* Scheme model for User, must be modified */
+/* Schema model for User, must be modified */
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  oauthId: { type: String, required: true },
+  provider: { type: String, required: true },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minLength: 6,
+    maxLength: 30,
+    trim: true,
+  },
+  email: { type: String, required: true, unique: true, trim: true },
+  createdAt: { type: Date, default: Date.now, immutable: true },
 })
 
+/* Create the User model, you need to create a model so that it can be used by controllers. This exports the model creation, which is more concise*/
 module.exports = mongoose.model('User', UserSchema)
