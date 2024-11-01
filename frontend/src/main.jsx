25a314fbc97a764eedd2f7fbde3b34a58620993a
Andrew Kim <andrew.igk@gmail.com>
2024-11-01 14:51:29 -0400

    Implement Google sign-in on front-end, verify insert in DB

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <GoogleOAuthProvider clientId="429300661908-109jdi18k4adpeilt3kja86trj8p48g9.apps.googleusercontent.com">
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </GoogleOAuthProvider>
 )
