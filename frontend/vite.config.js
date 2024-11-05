a2c5530280673c9557360d7d79e6d1bb4f29ca05
Andrew Kim <andrew.igk@gmail.com>
2024-11-04 22:48:10 -0500

    Set up dockerfiles

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  preview: {
+    port: 5173,
+  },
+  server: {
+    host: true,
+    port: 5173,
+    origin: 'http://0.0.0.0:5173', // Make sure this matches your exposed port
+  },
 })
