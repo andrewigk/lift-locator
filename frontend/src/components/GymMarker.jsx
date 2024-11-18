c25f93cc46bc38245983f82c8227d5bbc66450dc
Andrew Kim <andrew.igk@gmail.com>
2024-11-18 13:54:08 -0500

    Formatting improvements to geo control

--- frontend/src/components/GymMarker.jsx
+++ frontend/src/components/GymMarker.jsx
@@ -3,6 +3,39 @@ import { Marker } from 'react-map-gl/maplibre'
 import maplibregl from 'maplibre-gl'
 
 const GymMarker = ({ gymLocations }) => {
+  const handleMarkerClick = async (latitude, longitude, popup) => {
+    const result = await reverseGeocode(latitude, longitude)
+
+    popup.setHTML(`<div>Test ${result.properties.address?.road}</div>`)
+  }
+
+  const reverseGeocode = async (latitude, longitude) => {
+    const features = []
+    try {
+      const request = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=geojson&addressdetails=1`
+      const response = await fetch(request)
+      const geojson = await response.json()
+
+      if (geojson.features) {
+        for (const feature of geojson.features) {
+          const point = {
+            type: 'Feature',
+            geometry: feature.geometry,
+            place_name: feature.properties.display_name,
+            properties: feature.properties,
+            text: feature.properties.display_name,
+            place_type: ['place'],
+            center: feature.geometry.coordinates,
+          }
+          features.push(point)
+        }
+      }
+    } catch (e) {
+      console.error(`Failed to reverseGeocode with error: ${e}`)
+    }
+    return { features }
+  }
+
   return (
     <div>
       {gymLocations.map((gym, index) => {
@@ -39,6 +72,10 @@ const GymMarker = ({ gymLocations }) => {
             <p>Last Updated: ${gym.lastUpdated.split('T')[0]}</p>
             </div>
             </div>`)
+
+        popup.on('open', () => {
+          handleMarkerClick(gym.latitude, gym.longitude, popup)
+        })
 
         return (
           <Marker
